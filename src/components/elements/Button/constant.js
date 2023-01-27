c4dd5c03f43cdf082181e300ed3fb49d8d03892d
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2023-01-27 16:06:51 +0700

    Update button gray style

--- src/components/elements/Button/constant.js
+++ src/components/elements/Button/constant.js
@@ -1,7 +1,7 @@
 export const BUTTON_CLASS = {
   OUTLINE: {
     blue: " border-blue-500/20 text-blue-500 enabled:hover:text-white enabled:hover:bg-blue-500 enabled:hover:border-blue-500 focus:ring-blue-200 dark:focus:ring-offset-gray-800 disabled:text-blue-400 ",
-    gray: " border-gray-800/20 text-gray-900 dark:text-white enabled:hover:text-gray-900 enabled:hover:bg-gray-500 enabled:hover:border-gray-500 focus:ring-gray-200 dark:focus:ring-offset-gray-800 disabled:text-gray-400 ",
+    gray: " border-gray-800/20 dark:border-gray-500/20 text-gray-900 dark:text-white enabled:hover:text-white enabled:hover:bg-gray-500 enabled:hover:border-gray-500 focus:ring-gray-200 dark:focus:ring-offset-gray-800 disabled:text-gray-400 ",
     green:
       " border-green-500/20 text-green-500 enabled:hover:text-white enabled:hover:bg-green-500 enabled:hover:border-green-500 focus:ring-green-200 dark:focus:ring-offset-gray-800 disabled:text-green-400 ",
     purple:
@@ -33,15 +33,15 @@ export const BUTTON_CLASS = {
       " bg-yellow-500 text-white enabled:hover:bg-yellow-600 focus:ring-yellow-500 dark:focus:ring-offset-gray-800 disabled:bg-yellow-400 ",
   },
   TEXT: {
-    blue: " enabled:text-blue-500 enabled:hover:bg-blue-100 focus:ring-blue-500 dark:focus:ring-offset-gray-800 ",
-    gray: " enabled:text-gray-800 enabled:dark:text-white enabled:hover:bg-gray-100 enabled:hover:dark:bg-gray-800 focus:ring-gray-800 dark:focus:ring-offset-gray-800 ",
+    blue: " enabled:text-blue-500 enabled:hover:bg-blue-500/20 focus:ring-blue-500 dark:focus:ring-offset-gray-800 ",
+    gray: " enabled:text-gray-800 enabled:dark:text-white enabled:hover:bg-gray-800/20 enabled:hover:dark:bg-gray-500/20 focus:ring-gray-800 dark:focus:ring-offset-gray-800 ",
     green:
-      " enabled:text-green-500 enabled:hover:bg-green-100 focus:ring-green-500 dark:focus:ring-offset-gray-800 ",
+      " enabled:text-green-500 enabled:hover:bg-green-500/20 focus:ring-green-500 dark:focus:ring-offset-gray-800 ",
     purple:
-      " enabled:text-purple-500 enabled:hover:bg-purple-100 focus:ring-purple-500 dark:focus:ring-offset-gray-800 ",
-    red: " enabled:text-red-500 enabled:hover:bg-red-100 focus:ring-red-500 dark:focus:ring-offset-gray-800 ",
+      " enabled:text-purple-500 enabled:hover:bg-purple-500/20 focus:ring-purple-500 dark:focus:ring-offset-gray-800 ",
+    red: " enabled:text-red-500 enabled:hover:bg-red-500/20 focus:ring-red-500 dark:focus:ring-offset-gray-800 ",
     yellow:
-      " enabled:text-yellow-500 enabled:hover:bg-yellow-100 focus:ring-yellow-500 dark:focus:ring-offset-gray-800 ",
+      " enabled:text-yellow-500 enabled:hover:bg-yellow-500/20 focus:ring-yellow-500 dark:focus:ring-offset-gray-800 ",
   },
 };
 
