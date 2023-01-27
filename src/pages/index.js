9e6ea9950d4c1f45c246bb173381e457cafd9c1e
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2023-01-27 13:30:04 +0700

    Add button Guide

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,50 +3,98 @@ import CheckboxGroup from "@elements/CheckboxGroup";
 import Navbar from "@fragments/Navbar";
 import { useState } from "react";
 import Button from "@elements/Button";
+import Card from "@elements/Card";
 
 export default function Tess() {
   return (
     <div className="">
       <Navbar />
-      <div className="container py-4">
-        <div className="flex justify-between">
-          <Button color="blue">Solid</Button>
-          <Button color="gray">Solid</Button>
-          <Button color="green">Solid</Button>
-          <Button color="purple">Solid</Button>
-          <Button color="red">Solid</Button>
-          <Button color="yellow">Solid</Button>
-        </div>
-        <div className="flex justify-between">
-          <Button>Solid</Button>
-          <Button variant="soft">Soft</Button>
-          <Button variant="outline">Outline</Button>
-          <Button variant="text">Text</Button>
-        </div>
+      <div className="container flex gap-2 flex-col mt-2">
+        <ButtonSection />
         <CheckboxSection />
       </div>
     </div>
   );
 }
 
+export function ButtonSection() {
+  const buttonVariant = ["solid", "soft", "outline", "text"];
+  const buttonColor = ["blue", "gray", "green", "red", "purple", "yellow"];
+  const buttonSize = ["small", "medium", "large"];
+  const defaultStyle = "capitalize tracking-wider";
+
+  return (
+    <>
+      {buttonVariant.map((variant, i) => (
+        <Card className="flex flex-wrap gap-2" key={i}>
+          {buttonColor.map((color, i) => (
+            <Button
+              className={defaultStyle}
+              color={color}
+              key={i}
+              variant={variant}
+            >
+              {variant}
+            </Button>
+          ))}
+        </Card>
+      ))}
+      <Card className="flex flex-wrap gap-2 items-center">
+        {buttonSize.map((size, i) => (
+          <div key={i}>
+            <Button className={defaultStyle} size={size}>
+              {size}
+            </Button>
+          </div>
+        ))}
+      </Card>
+      <Card className="flex flex-wrap gap-2 items-center">
+        {buttonVariant.map((variant, i) => (
+          <div key={i}>
+            <Button className={defaultStyle} disabled variant={variant}>
+              {variant}
+            </Button>
+          </div>
+        ))}
+      </Card>
+      <Card className="flex flex-wrap gap-2 items-center">
+        {buttonVariant.map((variant, i) => (
+          <div key={i}>
+            <Button className={defaultStyle} loading variant={variant}>
+              {variant}
+            </Button>
+          </div>
+        ))}
+      </Card>
+    </>
+  );
+}
+
 export function CheckboxSection() {
-  const [value, setValue] = useState([]);
+  const [value, setValue] = useState(["1", "4"]);
 
   const options = [
     {
       description: "Ini Contoh Deskripsi",
       label: "Opsi Pertama",
-      value: "test1",
+      value: "1",
     },
     {
       description: "Ini Contoh Deskripsi",
       label: "Opsi Kedua",
-      value: "test2",
+      value: "2",
     },
     {
       description: "Ini Contoh Deskripsi",
+      disabled: true,
+      label: "Opsi Keempat",
+      value: "4",
+    },
+    {
+      description: "Ini Contoh Deskripsi",
+      disabled: true,
       label: "Opsi Ketiga",
-      value: "test3",
+      value: "3",
     },
   ];
 
@@ -60,9 +108,9 @@ export function CheckboxSection() {
   const getOptionLabel = (option) => {
     return option.label;
   };
-  // const getOptionDisabled = (option) => {
-  //   return option.disabled;
-  // };
+  const getOptionDisabled = (option) => {
+    return option?.disabled;
+  };
   const onChange = (option, val) => {
     const matchIndex = val.findIndex((v) => v === option.value);
     let newVal = [...val];
@@ -73,6 +121,7 @@ export function CheckboxSection() {
 
   const checkboxProps = {
     getOptionDescription,
+    getOptionDisabled,
     getOptionLabel,
     isChecked,
     onChange,
