09a9ea6974971ea26ab9b5d67f32e416a0ed6f82
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2023-01-28 08:29:03 +0700

    Check acordion not working

---
+++ src/pages/acordion.js
@@ -0,0 +1,178 @@
+import React from "react";
+
+export default function acordion() {
+  return (
+    <div className="hs-accordion-group">
+      <div className="hs-accordion active" id="hs-basic-heading-one">
+        <button
+          aria-controls="hs-basic-collapse-one"
+          className="hs-accordion-toggle hs-accordion-active:text-blue-600 group py-3 inline-flex items-center gap-x-3 w-full font-semibold text-left text-gray-800 transition hover:text-gray-500 dark:hs-accordion-active:text-blue-500 dark:text-gray-200 dark:hover:text-gray-400"
+        >
+          <svg
+            className="hs-accordion-active:hidden hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 block w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-gray-400"
+            fill="none"
+            height="16"
+            viewBox="0 0 16 16"
+            width="16"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2.62421 7.86L13.6242 7.85999"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeWidth="2"
+            />
+            <path
+              d="M8.12421 13.36V2.35999"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeWidth="2"
+            />
+          </svg>
+          <svg
+            className="hs-accordion-active:block hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 hidden w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-gray-400"
+            fill="none"
+            height="16"
+            viewBox="0 0 16 16"
+            width="16"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2.62421 7.86L13.6242 7.85999"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeWidth="2"
+            />
+          </svg>
+          Accordion #1
+        </button>
+        <div
+          aria-labelledby="hs-basic-heading-one"
+          className="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
+          id="hs-basic-collapse-one"
+        >
+          <p className="text-gray-800 dark:text-gray-200">
+            <em>This is the third item's accordion body.</em> It is hidden by
+            default, until the collapse plugin adds the appropriate classes that
+            we use to style each element. These classes control the overall
+            appearance, as well as the showing and hiding via CSS transitions.
+          </p>
+        </div>
+      </div>
+
+      <div className="hs-accordion" id="hs-basic-heading-two">
+        <button
+          aria-controls="hs-basic-collapse-two"
+          className="hs-accordion-toggle hs-accordion-active:text-blue-600 group py-3 inline-flex items-center gap-x-3 w-full font-semibold text-left text-gray-800 transition hover:text-gray-500 dark:hs-accordion-active:text-blue-500 dark:text-gray-200 dark:hover:text-gray-400"
+        >
+          <svg
+            className="hs-accordion-active:hidden hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 block w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-gray-400"
+            fill="none"
+            height="16"
+            viewBox="0 0 16 16"
+            width="16"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2.62421 7.86L13.6242 7.85999"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeWidth="2"
+            />
+            <path
+              d="M8.12421 13.36V2.35999"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeWidth="2"
+            />
+          </svg>
+          <svg
+            className="hs-accordion-active:block hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 hidden w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-gray-400"
+            fill="none"
+            height="16"
+            viewBox="0 0 16 16"
+            width="16"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2.62421 7.86L13.6242 7.85999"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeWidth="2"
+            />
+          </svg>
+          Accordion #2
+        </button>
+        <div
+          aria-labelledby="hs-basic-heading-two"
+          className="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
+          id="hs-basic-collapse-two"
+        >
+          <p className="text-gray-800 dark:text-gray-200">
+            <em>This is the third item's accordion body.</em> It is hidden by
+            default, until the collapse plugin adds the appropriate classes that
+            we use to style each element. These classes control the overall
+            appearance, as well as the showing and hiding via CSS transitions.
+          </p>
+        </div>
+      </div>
+
+      <div className="hs-accordion" id="hs-basic-heading-three">
+        <button
+          aria-controls="hs-basic-collapse-three"
+          className="hs-accordion-toggle hs-accordion-active:text-blue-600 group py-3 inline-flex items-center gap-x-3 w-full font-semibold text-left text-gray-800 transition hover:text-gray-500 dark:hs-accordion-active:text-blue-500 dark:text-gray-200 dark:hover:text-gray-400"
+        >
+          <svg
+            className="hs-accordion-active:hidden hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 block w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-gray-400"
+            fill="none"
+            height="16"
+            viewBox="0 0 16 16"
+            width="16"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2.62421 7.86L13.6242 7.85999"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeWidth="2"
+            />
+            <path
+              d="M8.12421 13.36V2.35999"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeWidth="2"
+            />
+          </svg>
+          <svg
+            className="hs-accordion-active:block hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 hidden w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-gray-400"
+            fill="none"
+            height="16"
+            viewBox="0 0 16 16"
+            width="16"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2.62421 7.86L13.6242 7.85999"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeWidth="2"
+            />
+          </svg>
+          Accordion #3
+        </button>
+        <div
+          aria-labelledby="hs-basic-heading-three"
+          className="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
+          id="hs-basic-collapse-three"
+        >
+          <p className="text-gray-800 dark:text-gray-200">
+            <em>This is the third item's accordion body.</em> It is hidden by
+            default, until the collapse plugin adds the appropriate classes that
+            we use to style each element. These classes control the overall
+            appearance, as well as the showing and hiding via CSS transitions.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
