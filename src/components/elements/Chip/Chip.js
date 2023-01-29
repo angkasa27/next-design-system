3a1864c3e8ded78560def6ce5c5d5b9fb718944c
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2023-01-29 22:33:43 +0700

    Add Icon component using lucide icon

--- src/components/elements/Chip/Chip.js
+++ src/components/elements/Chip/Chip.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import Button from "@elements/Button";
 import { CHIP_CLASS } from "./constant";
+import Icon from "@assets/Icon/Icon";
 
 export default function Chip({ children, onClose, className, color }) {
   return (
@@ -16,16 +17,7 @@ export default function Chip({ children, onClose, className, color }) {
       {children}
       {!!onClose && (
         <Button color={color} fluid rounded="full" variant="text">
-          <svg
-            className="h-4 w-4"
-            fill="currentColor"
-            height="16"
-            viewBox="0 0 16 16"
-            width="16"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-          </svg>
+          <Icon name="X" size={12} />
         </Button>
       )}
     </span>
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import Acordion from "@elements/Acordion";
 import Badge from "@elements/Badge";
 import List from "@elements/List";
 import Chip from "@elements/Chip";
+import Icon from "@assets/Icon/Icon";
 
 const COLORS = ["blue", "gray", "green", "red", "purple", "yellow"];
 
@@ -65,23 +66,14 @@ export function AcordionSection() {
 }
 
 export function AlertSection() {
+  const InfoIcon = <Icon name="Info" size={20} />;
+
   return (
     <ul>
       <List className="w-full">
         <Alert
           className="w-full"
-          icon={
-            <svg
-              className="h-4 w-4 mt-0.5 "
-              fill="currentColor"
-              height="16"
-              viewBox="0 0 16 16"
-              width="16"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
-            </svg>
-          }
+          icon={InfoIcon}
           id="alert1"
           title="Ini contoh alert dengan detail"
         >
@@ -99,18 +91,7 @@ export function AlertSection() {
             { name: "Detail", props: { onClick: () => console.log("detail") } },
           ]}
           className="w-full"
-          icon={
-            <svg
-              className="h-4 w-4 mt-0.5"
-              fill="currentColor"
-              height="16"
-              viewBox="0 0 16 16"
-              width="16"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
-            </svg>
-          }
+          icon={InfoIcon}
           id="alert2"
           title="Ini contoh alert dengan action detail"
         />
@@ -119,18 +100,7 @@ export function AlertSection() {
         <Alert
           className="w-full"
           dismissible
-          icon={
-            <svg
-              className="h-4 w-4 mt-0.5"
-              fill="currentColor"
-              height="16"
-              viewBox="0 0 16 16"
-              width="16"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
-            </svg>
-          }
+          icon={InfoIcon}
           id="alert3"
           title="Ini contoh alert close"
         />
@@ -151,55 +121,25 @@ export function AlertSection() {
 }
 
 export function BadgeSection() {
+  const BellIcon = <Icon name="Bell" />;
+
   return (
     <ul>
       <List className="flex flex-wrap gap-2 w-full">
         <Button className="relative">
-          <svg
-            fill="currentColor"
-            height="16"
-            viewBox="0 0 16 16"
-            width="16"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z" />
-          </svg>
+          {BellIcon}
           <Badge color="red" />
         </Button>
         <Button className="relative" variant="soft">
-          <svg
-            fill="currentColor"
-            height="16"
-            viewBox="0 0 16 16"
-            width="16"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z" />
-          </svg>
+          {BellIcon}
           <Badge color="red" ping />
         </Button>
         <Button className="relative">
-          <svg
-            fill="currentColor"
-            height="16"
-            viewBox="0 0 16 16"
-            width="16"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z" />
-          </svg>
+          {BellIcon}
           <Badge color="red">99+</Badge>
         </Button>
         <Button className="relative" variant="soft">
-          <svg
-            fill="currentColor"
-            height="16"
-            viewBox="0 0 16 16"
-            width="16"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z" />
-          </svg>
+          {BellIcon}
           <Badge color="red" ping>
             99+
           </Badge>
@@ -208,15 +148,7 @@ export function BadgeSection() {
       <List className="flex flex-wrap gap-2 w-full">
         {COLORS.map((color, i) => (
           <Button className="relative" key={i} variant="soft">
-            <svg
-              fill="currentColor"
-              height="16"
-              viewBox="0 0 16 16"
-              width="16"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z" />
-            </svg>
+            {BellIcon}
             <Badge color={color}></Badge>
           </Button>
         ))}
@@ -224,15 +156,7 @@ export function BadgeSection() {
       <List className="flex flex-wrap gap-2 w-full">
         {COLORS.map((color, i) => (
           <Button className="relative" key={i} variant="soft">
-            <svg
-              fill="currentColor"
-              height="16"
-              viewBox="0 0 16 16"
-              width="16"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z" />
-            </svg>
+            {BellIcon}
             <Badge color={color}>{color}</Badge>
           </Button>
         ))}
